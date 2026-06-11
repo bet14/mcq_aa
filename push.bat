a396c071f6a00cc7020a8e72da93e2364af2e7d5
bet14 <hoangthuylynh@gmail.com>
2026-06-11 21:00:32 +0200

    update 2026-06-11 21:00

--- push.bat
+++ push.bat
@@ -1,17 +1,4 @@
 @echo off
 cd /d "%~dp0"
-
-echo --- git status ---
-git status
-
-echo.
-set /p MSG="Commit message (leave blank for default): "
-if "%MSG%"=="" set MSG=update quiz files
-
-git add .
-git commit -m "%MSG%"
-git push origin main
-
-echo.
-echo Done.
+python push.py %*
 pause
